6373260717f32239bcea74a6d9b5dd9f5369c2b8
Michele Vargas <michele.vargas@levva.io>
2024-04-26 13:50:08 -0300

    criação do primeiro gráfico

--- meu_app.py
+++ meu_app.py
@@ -1,18 +1,26 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(page_title="Meu Site Streamlit")
 
 # Separação site é por containers e usa o with para isso
 with st.container():
-    st.subheader("Meu primeiro site com o streamlit")
+#    st.subheader("Meu primeiro site com o streamlit")
     st.title("Dashboard de contratos")
-    st.write("Informações sobre os contratos fechados pela Hash&Co ao longo de Maio")
-    st.write("Quere aprender Python? [Clique aqui](https://www.hashtagtreinamentos.com/curso-python)")
+#    st.write("Informações sobre os contratos fechados pela Hash&Co ao longo de Maio")
+#    st.write("Quer aprender Python? [Clique aqui](https://www.hashtagtreinamentos.com/curso-python)")
+
+
+# Decorators
+@st.cache_data
+def carregar_dados():
+    tabela = pd.read_csv("resultados.csv")
+    return tabela
 
 with st.container():
     st.write("---")
-
-with st.container():
-    st.write("Um teste de texto para ver atualização da pagina")
-    st.title("vou escrever um titulo e ver como ele fica aqui")
-    
+    qtde_dias = st.selectbox("Selecione o periodo", ["7D", "15D", "21D", "30D"])
+    num_dias = int(qtde_dias.replace("D", ""))
+    dados = carregar_dados()
+    dados = dados[-num_dias:]
+    st.area_chart(dados, x="Data", y="Contratos")
